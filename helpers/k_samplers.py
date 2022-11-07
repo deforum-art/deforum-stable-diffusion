d8dc00e5afa048b7a4df7362c380b03927c13f61
XmYx <miklos.mnagy@gmail.com>
2022-11-07 01:02:42 +0100

    k-samplers 0.10

--- helpers/k_samplers.py
+++ helpers/k_samplers.py
@@ -5,17 +5,17 @@ import torch
 
 
 def sampler_fn(
-    c: torch.Tensor,
-    uc: torch.Tensor,
-    args,
-    model_wrap: CompVisDenoiser,
-    init_latent: Optional[torch.Tensor] = None,
-    t_enc: Optional[torch.Tensor] = None,
-    device=torch.device("cpu")
-    if not torch.cuda.is_available()
-    else torch.device("cuda"),
-    cb: Callable[[Any], None] = None,
-    verbose: Optional[bool] = False,
+        c: torch.Tensor,
+        uc: torch.Tensor,
+        args,
+        model_wrap: CompVisDenoiser,
+        init_latent: Optional[torch.Tensor] = None,
+        t_enc: Optional[torch.Tensor] = None,
+        device=torch.device("cpu")
+        if not torch.cuda.is_available()
+        else torch.device("cuda"),
+        cb: Callable[[Any], None] = None,
+        verbose: Optional[bool] = False,
 ) -> torch.Tensor:
     shape = [args.C, args.H // args.f, args.W // args.f]
     sigmas: torch.Tensor = model_wrap.get_sigmas(args.steps)
@@ -23,8 +23,8 @@ def sampler_fn(
     if args.use_init:
         if len(sigmas) > 0:
             x = (
-                init_latent
-                + torch.randn([args.n_samples, *shape], device=device) * sigmas[0]
+                    init_latent
+                    + torch.randn([args.n_samples, *shape], device=device) * sigmas[0]
             )
         else:
             x = init_latent
@@ -41,6 +41,32 @@ def sampler_fn(
         "disable": False,
         "callback": cb,
     }
+    min = sigmas[0].item()
+    max = min
+    for i in sigmas:
+        if i.item() < min and i.item() != 0.0:
+            min = i.item()
+    if args.sampler in ["dpm_fast"]:
+        sampler_args = {
+            "model": model_wrap,
+            "x": x,
+            "sigma_min": min,
+            "sigma_max": max,
+            "extra_args": {"cond": c, "uncond": uc, "cond_scale": args.scale},
+            "disable": False,
+            "callback": cb,
+            "n":args.steps,
+        }
+    elif args.sampler in ["dpm_adaptive"]:
+        sampler_args = {
+            "model": model_wrap,
+            "x": x,
+            "sigma_min": min,
+            "sigma_max": max,
+            "extra_args": {"cond": c, "uncond": uc, "cond_scale": args.scale},
+            "disable": False,
+            "callback": cb,
+        }
     sampler_map = {
         "klms": sampling.sample_lms,
         "dpm2": sampling.sample_dpm_2,
@@ -48,6 +74,10 @@ def sampler_fn(
         "heun": sampling.sample_heun,
         "euler": sampling.sample_euler,
         "euler_ancestral": sampling.sample_euler_ancestral,
+        "dpm_fast": sampling.sample_dpm_fast,
+        "dpm_adaptive": sampling.sample_dpm_adaptive,
+        "dpmpp_2s_a": sampling.sample_dpmpp_2s_ancestral,
+        "dpmpp_2m": sampling.sample_dpmpp_2m,
     }
 
     samples = sampler_map[args.sampler](**sampler_args)
@@ -56,7 +86,7 @@ def sampler_fn(
 
 def make_inject_timing_fn(inject_timing, model, steps):
     """
-    inject_timing (int or list of ints or list of floats between 0.0 and 1.0): 
+    inject_timing (int or list of ints or list of floats between 0.0 and 1.0):
         int: compute every inject_timing steps
         list of floats: compute on these decimal fraction steps (eg, [0.5, 1.0] for 50 steps would be at steps 25 and 50)
         list of ints: compute on these steps
@@ -91,5 +121,4 @@ def make_inject_timing_fn(inject_timing, model, steps):
     else:
         raise Exception(f"Not a valid input: inject_timing={inject_timing}\n" +
                         f"Must be an int, list of all ints (between step 1 and {steps}), or list of all floats between 0.0 and 1.0")
-
     return timing_fn
