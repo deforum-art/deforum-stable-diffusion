7b75f20744c8c4bfe772a516f7b11ad64818cec3
Dreaming Tulpa <hey@dreamingtulpa.com>
2022-11-20 11:07:57 +0100

    remove 'google.colab' check in setup_environment()

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -44,89 +44,89 @@ def setup_environment():
         ipy = get_ipython()
     except:
         ipy = 'could not get_ipython'
-    if 'google.colab' in str(ipy):
-        print("..setting up environment")
-        start_time = time.time()
-        all_process = [
-            ['pip', 'install', 'torch==1.12.1+cu113', 'torchvision==0.13.1+cu113', '--extra-index-url', 'https://download.pytorch.org/whl/cu113'],
-            ['pip', 'install', 'omegaconf==2.2.3', 'einops==0.4.1', 'pytorch-lightning==1.7.4', 'torchmetrics==0.9.3', 'torchtext==0.13.1', 'transformers==4.21.2', 'kornia==0.6.7'],
-            ['git', 'clone', 'https://github.com/deforum-art/deforum-stable-diffusion'],
-            ['pip', 'install', 'accelerate', 'ftfy', 'jsonmerge', 'matplotlib', 'resize-right', 'timm', 'torchdiffeq','scikit-learn'],
-        ]
+
+    print("..setting up environment")
+    start_time = time.time()
+    all_process = [
+        ['pip', 'install', 'torch==1.12.1+cu113', 'torchvision==0.13.1+cu113', '--extra-index-url', 'https://download.pytorch.org/whl/cu113'],
+        ['pip', 'install', 'omegaconf==2.2.3', 'einops==0.4.1', 'pytorch-lightning==1.7.4', 'torchmetrics==0.9.3', 'torchtext==0.13.1', 'transformers==4.21.2', 'kornia==0.6.7'],
+        ['git', 'clone', 'https://github.com/deforum-art/deforum-stable-diffusion'],
+        ['pip', 'install', 'accelerate', 'ftfy', 'jsonmerge', 'matplotlib', 'resize-right', 'timm', 'torchdiffeq','scikit-learn'],
+    ]
+    for process in all_process:
+        running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if print_subprocess:
+            print(running)
+    with open('deforum-stable-diffusion/src/k_diffusion/__init__.py', 'w') as f:
+        f.write('')
+    sys.path.extend([
+        'deforum-stable-diffusion/',
+        'deforum-stable-diffusion/src',
+    ])
+    end_time = time.time()
+
+    if use_xformers:
+
+        print("..installing xformers")
+
+        all_process = [['pip', 'install', 'triton==2.0.0.dev20220701']]
         for process in all_process:
             running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
             if print_subprocess:
                 print(running)
-        with open('deforum-stable-diffusion/src/k_diffusion/__init__.py', 'w') as f:
-            f.write('')
-        sys.path.extend([
-            'deforum-stable-diffusion/',
-            'deforum-stable-diffusion/src',
-        ])
-        end_time = time.time()
 
-        if use_xformers:
-
-            print("..installing xformers")
-
-            all_process = [['pip', 'install', 'triton==2.0.0.dev20220701']]
-            for process in all_process:
-                running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
-                if print_subprocess:
-                    print(running)
-                    
-            v_card_name = subprocess.run(['nvidia-smi', '--query-gpu=name', '--format=csv,noheader'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-            if 't4' in v_card_name.lower():
-                name_to_download = 'T4'
-            elif 'v100' in v_card_name.lower():
-                name_to_download = 'V100'
-            elif 'a100' in v_card_name.lower():
-                name_to_download = 'A100'
-            elif 'p100' in v_card_name.lower():
-                name_to_download = 'P100'
-            elif 'a4000' in v_card_name.lower():
-                name_to_download = 'Non-Colab/Paperspace/A4000'
-            elif 'p5000' in v_card_name.lower():
-                name_to_download = 'Non-Colab/Paperspace/P5000'
-            elif 'quadro m4000' in v_card_name.lower():
-                name_to_download = 'Non-Colab/Paperspace/Quadro M4000'
-            elif 'rtx 4000' in v_card_name.lower():
-                name_to_download = 'Non-Colab/Paperspace/RTX 4000'
-            elif 'rtx 5000' in v_card_name.lower():
-                name_to_download = 'Non-Colab/Paperspace/RTX 5000'
-            else:
-                print(v_card_name + ' is currently not supported with xformers flash attention in deforum!')
-
-            if 'Non-Colab' in name_to_download:
-                x_ver = 'xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl'
-            else:
-                x_ver = 'xformers-0.0.13.dev0-py3-none-any.whl'
-
-            x_link = 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/precompiled/' + name_to_download + '/' + x_ver
-
-            all_process = [
-                ['wget', x_link],
-                ['pip', 'install', x_ver],
-                ['cp', 'deforum-stable-diffusion/src/ldm/modules/attention_xformers.py', 'deforum-stable-diffusion/src/ldm/modules/attention.py']
-            ]
-
-            for process in all_process:
-                running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
-                if print_subprocess:
-                    print(running)
-
-            print(f"Environment set up in {end_time-start_time:.0f} seconds")
+        v_card_name = subprocess.run(['nvidia-smi', '--query-gpu=name', '--format=csv,noheader'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if 't4' in v_card_name.lower():
+            name_to_download = 'T4'
+        elif 'v100' in v_card_name.lower():
+            name_to_download = 'V100'
+        elif 'a100' in v_card_name.lower():
+            name_to_download = 'A100'
+        elif 'p100' in v_card_name.lower():
+            name_to_download = 'P100'
+        elif 'a4000' in v_card_name.lower():
+            name_to_download = 'Non-Colab/Paperspace/A4000'
+        elif 'p5000' in v_card_name.lower():
+            name_to_download = 'Non-Colab/Paperspace/P5000'
+        elif 'quadro m4000' in v_card_name.lower():
+            name_to_download = 'Non-Colab/Paperspace/Quadro M4000'
+        elif 'rtx 4000' in v_card_name.lower():
+            name_to_download = 'Non-Colab/Paperspace/RTX 4000'
+        elif 'rtx 5000' in v_card_name.lower():
+            name_to_download = 'Non-Colab/Paperspace/RTX 5000'
         else:
-            all_process = [['cp', 'deforum-stable-diffusion/src/ldm/modules/attention_backup.py', 'deforum-stable-diffusion/src/ldm/modules/attention.py']]
-            for process in all_process:
-                running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
-                if print_subprocess:
-                    print(running)
+            print(v_card_name + ' is currently not supported with xformers flash attention in deforum!')
 
-            sys.path.extend([
-                'src'
-            ])
-        return
+        if 'Non-Colab' in name_to_download:
+            x_ver = 'xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl'
+        else:
+            x_ver = 'xformers-0.0.13.dev0-py3-none-any.whl'
+
+        x_link = 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/precompiled/' + name_to_download + '/' + x_ver
+
+        all_process = [
+            ['wget', x_link],
+            ['pip', 'install', x_ver],
+            ['cp', 'deforum-stable-diffusion/src/ldm/modules/attention_xformers.py', 'deforum-stable-diffusion/src/ldm/modules/attention.py']
+        ]
+
+        for process in all_process:
+            running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
+            if print_subprocess:
+                print(running)
+
+        print(f"Environment set up in {end_time-start_time:.0f} seconds")
+    else:
+        all_process = [['cp', 'deforum-stable-diffusion/src/ldm/modules/attention_backup.py', 'deforum-stable-diffusion/src/ldm/modules/attention.py']]
+        for process in all_process:
+            running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
+            if print_subprocess:
+                print(running)
+
+        sys.path.extend([
+            'src'
+        ])
+    return
 
 setup_environment()
 
