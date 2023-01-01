58d5a84e1cc46e1734176f04b25dcb1a76564cf7
reallybigname <reallybigname@gmail.com>
2022-12-31 17:45:05 -0800

    removed troublesome init_mse_image line
    forgot to delete in last pull request

--- helpers/render.py
+++ helpers/render.py
@@ -433,10 +433,6 @@ def render_animation(args, anim_args, animation_prompts, root):
                 mask_frame = os.path.join(args.outdir, 'maskframes', f"{frame_idx+1:05}.jpg")
                 args.mask_file = mask_frame
 
-        if anim_args.hybrid_video_use_video_as_mse_image:
-            args.init_mse_image = os.path.join(args.outdir, 'inputframes', f"{frame_idx:05}.jpg")
-            print(f"Using {args.init_mse_image} as init_mse_image")
-
         # sample the diffusion model
         sample, image = generate(args, root, frame_idx, return_latent=False, return_sample=True)
         # First image sample used for masking
