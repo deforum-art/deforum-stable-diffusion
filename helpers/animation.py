c2f9a4f1b10201c3e68ab3438cc3b1015711fe52
ajbt200128 <austin@cutedogs.org>
2023-01-23 13:38:25 -0500

    fix every frame

--- helpers/animation.py
+++ helpers/animation.py
@@ -42,7 +42,8 @@ def vid2frames(video_path, frames_path, n=1, overwrite=True):
         assert os.path.exists(video_path), f"Video input {video_path} does not exist"
 
         import subprocess
-        result = subprocess.run(["ffmpeg", "-i", video_path, "-r", "1/"+str(n), frames_path + "/%05d.jpg"], stdout=subprocess.PIPE)
+        result = subprocess.run(["ffmpeg", "-i", video_path, "-vf", f"select=not(mod(n,{n}))", "-vsync","vfr", frames_path + "/%05d.jpg"])
+        print(result.stdout)
     else: print("Frames already unpacked")
 
 # https://en.wikipedia.org/wiki/Rotation_matrix
