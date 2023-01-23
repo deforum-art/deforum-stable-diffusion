49d824fbb0d5b95d3da43ce7de73d6d128c4fd92
ajbt200128 <austin@cutedogs.org>
2023-01-23 13:07:14 -0500

    fix

--- helpers/animation.py
+++ helpers/animation.py
@@ -42,17 +42,19 @@ def vid2frames(video_path, frames_path, n=1, overwrite=True):
         assert os.path.exists(video_path), f"Video input {video_path} does not exist"
           
         vidcap = cv2.VideoCapture(video_path)
-        success,image = vidcap.read()
-        print(success, image, video_path)
-        count = 0
-        t=1
-        success = True
-        while success:
-            if count % n == 0:
-                cv2.imwrite(frames_path + os.path.sep + f"{t:05}.jpg" , image)     # save frame as JPEG file
-                t += 1
+        print(vidcap.isOpened())
+        while vidcap.isOpened():
             success,image = vidcap.read()
-            count += 1
+            print(success, image, video_path)
+            count = 0
+            t=1
+            success = True
+            while success:
+                if count % n == 0:
+                    cv2.imwrite(frames_path + os.path.sep + f"{t:05}.jpg" , image)     # save frame as JPEG file
+                    t += 1
+                success,image = vidcap.read()
+                count += 1
         print("Converted %d frames" % count)
     else: print("Frames already unpacked")
 
