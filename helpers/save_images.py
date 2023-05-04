4d40e623c2d63371ab6faa157bb76d81af45916c
Julian Harris <julian.harris@gmail.com>
2023-05-04 22:57:12 +0100

    Alter output directory so a run has its images batched together

--- helpers/save_images.py
+++ helpers/save_images.py
@@ -7,7 +7,7 @@ import time
 
 
 def get_output_folder(output_path, batch_folder):
-    out_path = os.path.join(output_path,time.strftime('%Y-%m'))
+    out_path = os.path.join(output_path,time.strftime('%y%m%d-%H%M'))
     if batch_folder != "":
         out_path = os.path.join(out_path, batch_folder)
     os.makedirs(out_path, exist_ok=True)
