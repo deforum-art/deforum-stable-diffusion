ac1283033fd8f23539777191cc6065479e812d64
deforum <110359942+deforum@users.noreply.github.com>
2024-07-27 17:20:33 -0700

    remove multichannel from histogram color matching

--- helpers/colors.py
+++ helpers/colors.py
@@ -7,10 +7,10 @@ def maintain_colors(prev_img, color_match_sample, mode):
     elif mode == 'Match Frame 0 HSV':
         prev_img_hsv = cv2.cvtColor(prev_img, cv2.COLOR_RGB2HSV)
         color_match_hsv = cv2.cvtColor(color_match_sample, cv2.COLOR_RGB2HSV)
-        matched_hsv = match_histograms(prev_img_hsv, color_match_hsv, multichannel=True)
+        matched_hsv = match_histograms(prev_img_hsv, color_match_hsv)
         return cv2.cvtColor(matched_hsv, cv2.COLOR_HSV2RGB)
     else: # Match Frame 0 LAB
         prev_img_lab = cv2.cvtColor(prev_img, cv2.COLOR_RGB2LAB)
         color_match_lab = cv2.cvtColor(color_match_sample, cv2.COLOR_RGB2LAB)
-        matched_lab = match_histograms(prev_img_lab, color_match_lab, multichannel=True)
+        matched_lab = match_histograms(prev_img_lab, color_match_lab)
         return cv2.cvtColor(matched_lab, cv2.COLOR_LAB2RGB)
